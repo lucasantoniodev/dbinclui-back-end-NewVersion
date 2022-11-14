f221004801b57cd425583225c834d856ba274296
lucasantoniodev <lucasantoniodev@gmail.com>
2022-11-14 09:07:33 -0300

    feat: create digital content validator and middleware

---
+++ src/middlewares/digitalContents/digitalContentRequestMiddleware.ts
@@ -0,0 +1,43 @@
+import { NextFunction, Request, Response } from "express";
+import { validationResult } from "express-validator";
+import { getByIdController } from "../../controllers/categories/GetByIdCategoryController.js";
+import { CategoryMongoRepository } from "../../repositories/mongoRepositories/CategoryMongoRepository.js";
+import { GuideMongoRepository } from "../../repositories/mongoRepositories/GuideMongoRepository.js";
+import { clientErrorResponse } from "../../responses/appResponses.js";
+import { GetByIdCategoryService } from "../../services/categories/GetByIdCategoryService.js";
+import { GetByIdGuideService } from "../../services/guides/GetByIdGuideService.js";
+
+export const digitalContentRequestMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const errorMessage = errors.array();
+    return clientErrorResponse(res, errorMessage);
+  }
+  const { guide, category } = req.body;
+
+  if (req.method === "POST" || (req.method === "PUT" && guide)) {
+    const guideRepository = new GuideMongoRepository();
+    const guideService = new GetByIdGuideService(guideRepository);
+
+    const resultGuide = await guideService.execute(guide);
+    if (resultGuide instanceof Error) {
+      return clientErrorResponse(res, resultGuide);
+    }
+  }
+
+  if (req.method === "POST" || (req.method === "PUT" && category)) {
+    const categoryRepository = new CategoryMongoRepository();
+    const categoryService = new GetByIdCategoryService(categoryRepository);
+
+    const resultCategory = await categoryService.execute(category);
+    if (resultCategory instanceof Error) {
+      return clientErrorResponse(res, resultCategory);
+    }
+  }
+
+  next();
+};
---
+++ src/middlewares/digitalContents/digitalContentRequestValidator.ts
@@ -0,0 +1,33 @@
+import { body, param } from "express-validator";
+
+export const digitalContentRequestValidator = (method: "post" | "put" | "get" | "delete") => {
+  if (method === "post") {
+    return [
+      body("title")
+        .notEmpty()
+        .withMessage("O título do guia não pode está vazio")
+        .isLength({ min: 1, max: 32 })
+        .withMessage("O título deve ter entre 1 a 32 caracteres")
+        .isString()
+        .withMessage("O formato deve ser uma string"),
+
+      body("shortDescription")
+        .notEmpty()
+        .withMessage("A descrição do conteúdo não pode está vazio")
+        .isString()
+        .withMessage("O formato deve ser uma string"),
+    ];
+  }
+
+  if (method === "put" || method === "get" || method === "delete") {
+    return [
+      param("id")
+        .notEmpty()
+        .withMessage("Um ID do conteúdo é necessário para esse método")
+        .isMongoId()
+        .withMessage("Formato de ID inválido"),
+    ];
+  }
+
+  return [];
+};
